77e5ef1b9eeecacd25f8708f8e832dc578846039
Wajid Korkani <errormrcoder@gmail.com>
2024-11-17 23:19:18 +0500

    Still web page is not responsive.

---
+++ Src/index.js
@@ -0,0 +1,3 @@
+let navSelectKey = document.getElementById('box1').innerText = "Text";
+let selectKeyLabel = document.getElementById("selectKeyLabel")
+selectKeyLabel.addEventListener('click', e => e.target.selectKeyLabel.innerText = "Zer0")
